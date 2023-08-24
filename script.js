2895d34ba4077129d3d6fe4d12d06ea5d4551b5c
Buragadda SriCharan <83228959+BuragaddaSriCharan@users.noreply.github.com>
2023-08-24 15:58:16 +0530

    script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
   // typing text animation script
   var typed = new Typed(".typing", {
-      strings: ["Automobile Designing", "Basic coding", "Analysing Automobile and structural objects"],
+      strings: ["Automobile Designing", "Basic coding", "Front end development and HR recruiter"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["Automobile Designing", "Basic coding", "Analysing Automobile and structural objects"],
+      strings: ["Automobile Designing", "Basic coding", "Front end development and HR recruiter"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
